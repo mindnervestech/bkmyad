30677518bc468516d9979d44ecb435e7666686e6
MindNerves <mindnervestech@gmail.com>
2014-09-11 01:31:09 +0530

    ccaavenue integration components

---
+++ app/controllers/CCAvenueController.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import java.util.zip.Adler32;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import viewmodel.CCAvenueDefaultVM;
+
+import com.ccavenue.security.AesCryptUtil;
+
+public class CCAvenueController extends Controller {
+	public static Result ccavenue() {
+    	CCAvenueDefaultVM ccAvenueDefaultVo = new CCAvenueDefaultVM();
+    	ccAvenueDefaultVo.Checksum = getChecksum(
+    			ccAvenueDefaultVo.Merchant_Id, ccAvenueDefaultVo.Order_Id, 
+    			ccAvenueDefaultVo.Amount, ccAvenueDefaultVo.Redirect_Url, ccAvenueDefaultVo.WorkingKey);
+        return ok(views.html.ccacheckout.render("Your new application is ready.", ccAvenueDefaultVo));
+    }
+    
+    public static Result redirect() {
+    	String WorkingKey = "3vrz1tf22sk3qcgh4gjvijd1fuqdup0f" ; //put in the 32 bit working key in the quotes provided here
+    	String encResponse=request().getQueryString("encResponse");
+    	AesCryptUtil aesUtil=new AesCryptUtil(WorkingKey);
+    	String ccaResponse=aesUtil.decrypt(encResponse);	
+    	return ok(views.html.ccaredirect.render("Your new application is ready. " + ccaResponse));
+    }
+    
+    private static String verifyChecksum(String MerchantId , String OrderId, String Amount, String AuthDesc, String WorkingKey, String CheckSum) throws Exception 	{
+    	String str = MerchantId+"|"+OrderId+"|"+Amount+"|"+AuthDesc+"|"+WorkingKey;
+    	
+    	Adler32  adl = new Adler32();                 
+    	adl.update(str.getBytes());
+    	long adler = adl.getValue();
+
+    	String newChecksum = String.valueOf(adl.getValue());
+    	return (newChecksum.equals(CheckSum)) ? "true" : "false";
+    	
+    }
+
+
+    private static String getChecksum(String MerchantId, String OrderId, String Amount, String redirectUrl, String WorkingKey)  {
+    	String str = MerchantId + "|" + OrderId + "|" + Amount + "|" + redirectUrl + "|" + WorkingKey;
+
+    	Adler32  adl = new Adler32();
+    	adl.update(str.getBytes());
+    	return String.valueOf(adl.getValue());
+    }
+
+}
---
+++ app/viewmodel/CCAvenueDefaultVM.java
@@ -0,0 +1,31 @@
+package viewmodel;
+
+import java.util.UUID;
+
+public class CCAvenueDefaultVM {
+	public String Merchant_Id = "M_raj28780_28780"; //This id(also User_Id)  available at "Generate Working Key" of "Settings & Options"
+	public String Amount = "10.0"; //your script should substitute the amount here in the quotes provided here
+	public String Order_Id = UUID.randomUUID().toString();//request.getParameter("order_id");  //your script should substitute the unique order description here in the quotes provided here
+	public String Redirect_Url = "http://106.186.119.78:9000/redirect";//your redirect URL where your customer will be redirected after authorisation from CCAvenue
+	public String WorkingKey = "3vrz1tf22sk3qcgh4gjvijd1fuqdup0f";   
+	//put in the 32 bit alphanumeric key in the quotes provided here.Please note that get this key login to your CCAvenue merchant account and visit the "Generate Working Key" section at the "Settings & Options" page. 
+	public String Checksum = "";
+	
+	public String billing_cust_name="Test";
+	public String billing_cust_address="Test";
+	public String billing_cust_country="India";
+	public String billing_cust_tel="9898898898";
+	public String billing_cust_email="test@test.info";
+	public String billing_cust_state="MH";
+	public String delivery_cust_name="test";
+	public String delivery_cust_address="test";
+	public String delivery_cust_country="India";
+	public String delivery_cust_state="MH";
+	public String delivery_cust_tel="9898989898";
+	public String delivery_cust_notes="No notes";
+	public String Merchant_Param="Test"; 
+	public String billing_city = "Mumbai";
+	public String billing_zip = "400059";
+	public String delivery_city = "Mumbai";
+	public String delivery_zip = "400059";
+}
