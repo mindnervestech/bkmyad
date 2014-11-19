1b064058164f5dc9ed6e1b0e8bbd749bf29df1ad
akashshinde44comp <akashshinde44comp@gmail.com>
2014-11-19 16:52:48 +0530

    global.scala file rm

--- app/Global.scala
+++
@@ -1,8 +0,0 @@
-import play.api._
-import play.api.mvc._
-import play.filters.gzip.GzipFilter
-
-object Global extends WithFilters(new GzipFilter(shouldGzip = (request, response) =>
-  response.headers.get("Content-Type").exists(_.startsWith("image/png")))) with GlobalSettings {
-  // onStart, onStop etc...
-}
