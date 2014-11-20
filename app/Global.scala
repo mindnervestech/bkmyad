8dca388fe421e6532be8bd362d6401cbdb020e59
akashshinde44comp <akashshinde44comp@gmail.com>
2014-11-20 20:08:12 +0530

    page not found

--- app/Global.scala
+++ app/Global.scala
@@ -1,7 +1,20 @@
 import play.api._
 import play.api.mvc._
 import play.filters.gzip.GzipFilter
-
+import play.api.mvc.Results._
+import scala.concurrent.Future
+import play.api.mvc.Result
+import play.api.mvc.SimpleResult._
 object Global extends WithFilters(new GzipFilter()) with GlobalSettings {
   // onStart, onStop etc...
+  override def onHandlerNotFound(request: RequestHeader) = {
+    Future.successful(NotFound(
+      views.html.notfound()
+    ))
+  }
+    
 }
+
+
+ 
+ 
