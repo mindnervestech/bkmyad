068d92f62dc0208d785b989efb72be8cef79f00c
akashshinde44comp <akashshinde44comp@gmail.com>
2014-12-01 17:13:19 +0530

    js new added

--- app/controllers/DispFrameController.java
+++ app/controllers/DispFrameController.java
@@ -1,16 +1,13 @@
 package controllers;
 
-
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.dispcalssifiedcontroller;
 
-public class DispFrameController  extends Controller{
+public class DispFrameController extends Controller {
 
-	
-	
 	public static Result index() {
-		
-	    return ok(dispcalssifiedcontroller.render() );
-		}
+
+		return ok(dispcalssifiedcontroller.render());
+	}
 }
